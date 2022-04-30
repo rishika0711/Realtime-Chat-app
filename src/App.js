36469cf69a663966b3a4422afdee7d40fe935fa2
Rishika Singh <rishikasingh0711@gmail.com>
2022-04-30 10:55:06 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="Chat-App">
+      <header className="header">
+        <div className='logo'>
+          <i class="fa-solid fa-comments"></i>
+        </div>
+        <nav className='primary-nav'>
+          <ul className='nav-items'>
+
+            <li className='nav-item'>Home</li>
+            <li className='nav-item'>Contact</li>
+            <li className='nav-item'>SignIn</li>
+            <li className='nav-item'>Chat</li>
+          </ul>
+        </nav>
+      </header>
+      
+      <div className='App-Body'>
+        <div className='chat-wrapper'>
+        <div className='chat-container'>
+          {/* 1st column */}
+          <div className='col col-1'>
+             <nav class="menu">
+        <ul class="items">
+          <li class="item">
+            <i class="fa fa-home" ></i>
+          </li>
+          <li class="item">
+            <i class="fa fa-user"></i>
+          </li>
+          <li class="item">
+            <i class="fa fa-pencil" ></i>
+          </li>
+          <li class="item item-active">
+            <i class="fa fa-commenting"></i>
+          </li>
+          <li class="item">
+            <i class="fa fa-file"></i>
+          </li>
+          <li class="item">
+            <i class="fa fa-cog"></i>
+          </li>
+        </ul>
+      </nav>
+          </div>
+          {/* 2st column */}
+          <div className='col col-2'>
+          {/* <section class="discussions">
+        <div class="discussion search">
+          <div class="searchbar">
+            <i class="fa fa-search" aria-hidden="true"></i>
+            <input type="text" placeholder="Search..."></input>
+          </div>
+        </div>
+        <div class="discussion message-active">
+          <div class="photo" align="background-image: url(https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80);">
+            <div class="online"></div>
+          </div>
+          <div class="desc-contact">
+            <p class="name">Megan Leib</p>
+            <p class="message">9 pm at the bar if possible 😳</p>
+          </div>
+          <div class="timer">12 sec</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://i.pinimg.com/originals/a9/26/52/a926525d966c9479c18d3b4f8e64b434.jpg);">
+            <div class="online"></div>
+          </div>
+          <div class="desc-contact">
+            <p class="name">Dave Corlew</p>
+            <p class="message">Let's meet for a coffee or something today ?</p>
+          </div>
+          <div class="timer">3 min</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=667&q=80);">
+          </div>
+          <div class="desc-contact">
+            <p class="name">Jerome Seiber</p>
+            <p class="message">I've sent you the annual report</p>
+          </div>
+          <div class="timer">42 min</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://card.thomasdaubenton.com/img/photo.jpg);">
+            <div class="online"></div>
+          </div>
+          <div class="desc-contact">
+            <p class="name">Thomas Dbtn</p>
+            <p class="message">See you tomorrow ! 🙂</p>
+          </div>
+          <div class="timer">2 hour</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://images.unsplash.com/photo-1553514029-1318c9127859?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80);">
+          </div>
+          <div class="desc-contact">
+            <p class="name">Elsie Amador</p>
+            <p class="message">What the f**k is going on ?</p>
+          </div>
+          <div class="timer">1 day</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://images.unsplash.com/photo-1541747157478-3222166cf342?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80);">
+          </div>
+          <div class="desc-contact">
+            <p class="name">Billy Southard</p>
+            <p class="message">Ahahah 😂</p>
+          </div>
+          <div class="timer">4 days</div>
+        </div>
+
+        <div class="discussion">
+          <div class="photo" align="background-image: url(https://images.unsplash.com/photo-1435348773030-a1d74f568bc2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80);">
+            <div class="online"></div>
+          </div>
+          <div class="desc-contact">
+            <p class="name">Paul Walker</p>
+            <p class="message">You can't see me</p>
+          </div>
+          <div class="timer">1 week</div>
+        </div>
+      </section> */}
+          </div>
+          {/* 3st column */}
+          <div className='col col-3'></div>
+        </div>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
